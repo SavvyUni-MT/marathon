4de081c58f803cfd263811dac54db447328dbd85
Savvyuni MT <savvyunimt@gmail.com>
2024-03-26 21:54:01 -0400

    initial commit

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' Favorite color of various people data
+#'
+#' @description \code{marathon} returns a dataframe
+#' of the favorite color of various people.
+#'
+#' @return Returns a dataframe of the favorite color
+#' of various people.
+#'
+#' @docType data
+#'
+#' @usage data(raw_data)
+#'
+#' @format A dataframe of individual-level observations
+#' with the following variables:
+#'
+#' \describe{
+#' \item{\code{name}}{A character vector of individual names.}
+#' \item{\code{fav_color}}{A character vector of colors.}
+#' }
+#'
+#' @keywords datasets
+#'
+#' @source \url{tellingstorieswithdata.com/10-store_and_share.html}
+#'
+"raw_data"
